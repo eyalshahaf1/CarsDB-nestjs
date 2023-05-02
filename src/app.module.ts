783007460b349b9220ad71cf121603b26fe340c3
eyalshahaf1 <eyal.shahaf1@gmail.com>
2023-05-02 19:00:06 +0300

    initial project

---
+++ src/app.module.ts
@@ -0,0 +1,59 @@
+import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
+import { APP_PIPE } from '@nestjs/core';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { DBOptions } from '../db.datasourceoptions';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { AppDataSource } from '../db.datasource';
+const cookieSession = require('cookie-session');
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (config: ConfigService) => {
+        const dbOptions: TypeOrmModuleOptions = {
+          // retryAttempts: 10,
+          // retryDelay: 3000,
+          // autoLoadEntities: false
+        };
+
+        Object.assign(dbOptions, DBOptions);
+
+        return dbOptions;
+      },
+    }),
+    UsersModule, 
+    ReportsModule
+  ],
+
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      }),
+    },
+  ],
+})
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        cookieSession({
+          keys: ['asdfasfd'],
+        }),
+      )
+      .forRoutes('*');
+  }
+}
